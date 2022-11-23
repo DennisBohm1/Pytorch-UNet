14c6970a2722936c436eeccfa4cb075b48dec071
DennisBohm1 <d.bohm@student.tudelft.nl>
2022-11-23 17:42:25 +0100

    Use MONAI UNet

--- utils/data_loading.py
+++ utils/data_loading.py
@@ -1,3 +1,4 @@
+import glob
 import logging
 from os import listdir
 from os.path import splitext
@@ -6,7 +7,65 @@ from pathlib import Path
 import numpy as np
 import torch
 from PIL import Image
-from torch.utils.data import Dataset
+from monai.data import DataLoader, Dataset, ITKReader, ImageDataset, CacheDataset
+from monai.transforms import *
+from torch.utils.data import random_split
+
+
+def create_data_loader(dir_img, dir_mask, val_percent, batch_size):
+    reader = ITKReader()
+    combined_list = []
+    for img in listdir(Path(dir_img)):
+        if img.endswith('.nii.gz'):
+            combined_list.append({
+                'image': str(Path(dir_img)) + '/' + str(img),
+                'mask': str(Path(dir_mask)) + '/label' + str(img),
+            })
+
+
+
+    test_transforms = Compose(
+        [
+            LoadImaged(keys="image"),  # d, h, w
+            # AddChanneld(keys="image"),  # c, d, h, w
+            # Transposed(keys="image", indices=[0, 2, 3, 1]),  # c, w, h, d
+            # Lambdad(keys="image", func=lambda x: x / x.max()),
+            #         SpatialPadd(keys="image", spatial_size=cfg.img_size),  # in case less than 80 slices
+            EnsureTyped(keys="image", dtype=torch.float32),
+            LoadImaged(keys="mask"),  # d, h, w
+            # AddChanneld(keys="mask"),  # c, d, h, w
+            # Transposed(keys="mask", indices=[0, 2, 3, 1]),  # c, w, h, d
+            # Lambdad(keys="image", func=lambda x: x / x.max()),
+            #         SpatialPadd(keys="image", spatial_size=cfg.img_size),  # in case less than 80 slices
+            EnsureTyped(keys="mask", dtype=torch.float32),
+        ]
+    )
+
+    # dataset = ImageDataset(
+    #     image_files=glob.glob('../data/NIH/Images/*.nii.gz'),
+    #     seg_files=glob.glob('../data/NIH/Mask/*.nii.gz'),
+    #     transform=test_transforms,
+    #     # seg_transform=test_transforms
+    # )
+
+    dataset = Dataset(combined_list, transform=test_transforms)
+
+    # 2. Split into train / validation partitions
+    n_val = int(len(dataset) * val_percent)
+    n_train = len(dataset) - n_val
+    train_set, val_set = random_split(
+        dataset, [n_train, n_val], generator=torch.Generator().manual_seed(0)
+    )
+
+    # 3. Create data loaders
+    train_loader = DataLoader(train_set, shuffle=True, batch_size=batch_size, num_workers=0, pin_memory=True)
+    val_loader = DataLoader(val_set, shuffle=False, drop_last=True, batch_size=batch_size, num_workers=0)
+
+    # for batch in train_loader:
+    #     images = batch[0]
+    #     true_masks = batch[1]
+
+    return train_loader, val_loader
 
 
 class BasicDataset(Dataset):
@@ -27,21 +86,41 @@ class BasicDataset(Dataset):
 
     @staticmethod
     def preprocess(pil_img, scale, is_mask):
-        w, h = pil_img.size
-        newW, newH = int(scale * w), int(scale * h)
-        assert newW > 0 and newH > 0, 'Scale is too small, resized images would have no pixel'
-        pil_img = pil_img.resize((newW, newH), resample=Image.NEAREST if is_mask else Image.BICUBIC)
+        # w, h = pil_img.size
+        # newW, newH = int(scale * w), int(scale * h)
+        # assert newW > 0 and newH > 0, 'Scale is too small, resized images would have no pixel'
+        # pil_img = pil_img.resize((newW, newH), resample=Image.NEAREST if is_mask else Image.BICUBIC)
         img_ndarray = np.asarray(pil_img)
 
-        if not is_mask:
-            if img_ndarray.ndim == 2:
-                img_ndarray = img_ndarray[np.newaxis, ...]
-            else:
-                img_ndarray = img_ndarray.transpose((2, 0, 1))
-
-            img_ndarray = img_ndarray / 255
+        # if not is_mask:
+        #     if img_ndarray.ndim == 2:
+        #         img_ndarray = img_ndarray[np.newaxis, ...]
+        #     else:
+        #         img_ndarray = img_ndarray.transpose((2, 0, 1))
+        #
+        #     img_ndarray = img_ndarray / 255
 
         return img_ndarray
+
+    def load_images(fs, N, Nchannels, ImSize):
+        if N < 0:
+            N = fs.__len__()
+
+        X = np.ndarray(shape=(N, Nchannels, ImSize, ImSize), dtype=np.uint8)
+        Y = np.ndarray(shape=(N, 1), dtype=np.uint8)
+        for i in range(0, N):
+            image = Image.open(fs[i], 'r')
+            X[i, :, :, :] = np.swapaxes(image, 0, 2)
+
+            # print extension
+            Y[i] = 1
+
+            # progress
+            if i % 10000 is 0:
+                print
+                "{0} of {1}: {2}".format(i, N, fs[i])
+
+        return (X, Y, fs[0:N])
 
     @staticmethod
     def load(filename):
@@ -55,23 +134,23 @@ class BasicDataset(Dataset):
 
     def __getitem__(self, idx):
         name = self.ids[idx]
-        mask_file = list(self.masks_dir.glob(name + self.mask_suffix + '.*'))
+        mask_file = list(self.masks_dir.glob(name + '.*'))
         img_file = list(self.images_dir.glob(name + '.*'))
 
         assert len(img_file) == 1, f'Either no image or multiple images found for the ID {name}: {img_file}'
         assert len(mask_file) == 1, f'Either no mask or multiple masks found for the ID {name}: {mask_file}'
-        mask = self.load(mask_file[0])
-        img = self.load(img_file[0])
+        mask = self.load_images(mask_file[0], 3, 1, )
+        img = self.load_images(img_file[0])
 
-        assert img.size == mask.size, \
-            f'Image and mask {name} should be the same size, but are {img.size} and {mask.size}'
+        # assert img.size == mask.size, \
+        #     f'Image and mask {name} should be the same size, but are {img.size} and {mask.size}'
 
-        img = self.preprocess(img, self.scale, is_mask=False)
-        mask = self.preprocess(mask, self.scale, is_mask=True)
+        # mask = self.preprocess(mask_file, self.scale, is_mask=True)
+        # img = self.preprocess(img_file, self.scale, is_mask=False)
 
         return {
-            'image': torch.as_tensor(img.copy()).float().contiguous(),
-            'mask': torch.as_tensor(mask.copy()).long().contiguous()
+            'image': torch.as_tensor(img_file.copy()).float().contiguous(),
+            'mask': torch.as_tensor(mask_file.copy()).long().contiguous()
         }
 
 
